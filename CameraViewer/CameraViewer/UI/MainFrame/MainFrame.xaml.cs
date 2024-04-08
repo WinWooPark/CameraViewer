3ff931c485597786d9abc2ec8f81441a984a0148
DESKTOP-MS05L9A\IntekPlus <seungwoopark0507@gmail.com>
2024-04-08 16:25:00 +0900

    프로젝트 재 생성

---
+++ CameraViewer/CameraViewer/UI/MainFrame/MainFrame.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+
+namespace CameraViewer.UI.MainFrame
+{
+    /// <summary>
+    /// MainFrame.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainFrame : UserControl
+    {
+        MainFrameViewModel _mainFrameViewModel;
+        public MainFrame()
+        {
+            _mainFrameViewModel = new MainFrameViewModel();
+            InitializeComponent();
+            DataContext = _mainFrameViewModel;
+        }
+    }
+}
---
+++ CameraViewer/CameraViewer/UI/MainFrame/MainFrameViewModel.cs
@@ -0,0 +1,78 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows.Media;
+
+namespace CameraViewer.UI.MainFrame
+{
+    public class MainFrameViewModel : ObservableObject
+    {
+        public MainFrameViewModel()
+        {
+            CameraState = Brushes.Red;
+            LightState = Brushes.Red;
+            MotionState = Brushes.Red;
+        }
+
+        private SolidColorBrush _cameraState;
+        public SolidColorBrush CameraState
+        {
+            get { return _cameraState; }
+            set
+            {
+                _cameraState = value;
+                OnPropertyChanged(nameof(_cameraState));
+            }
+        }
+
+        private SolidColorBrush _lightState;
+        public SolidColorBrush LightState
+        {
+            get { return _lightState; }
+            set
+            {
+                _lightState = value;
+                OnPropertyChanged(nameof(_lightState));
+            }
+        }
+
+        private SolidColorBrush _motionState;
+        public SolidColorBrush MotionState
+        {
+            get { return _motionState; }
+            set
+            {
+                _motionState = value;
+                OnPropertyChanged(nameof(_motionState));
+            }
+        }
+
+        private RelayCommand _programExit;
+        public RelayCommand ProgramExit 
+        {
+            get { return _programExit; }
+            set 
+            { 
+                _programExit = value;
+                OnPropertyChanged(nameof(_lightState));
+            }
+        }
+
+
+        public void CreateCommand() 
+        {
+            //ProgramExit = new RelayCommand();
+        }
+
+        public void HardWareState(bool cameraState, bool lightState, bool motionState) 
+        {
+            if (cameraState) CameraState = Brushes.Green;
+            else CameraState = Brushes.Red;
+
+            if (lightState) LightState = Brushes.Green;
+            else LightState = Brushes.Red;
+
+            if (motionState) MotionState = Brushes.Green;
+            else MotionState = Brushes.Red;
+        }
+    }
+}
